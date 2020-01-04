7019302feeea35d9bcc46e176f34cd94b6813ef6
BennoAlif <bennoalif41@gmail.com>
2020-01-04 15:29:52 +0700

    Authentication Middleware

--- functions/index.js
+++ functions/index.js
@@ -44,11 +44,44 @@ app.get("/screams", (req, res) => {
     });
 });
 
+const FBAuth = (req, res, next) => {
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    idToken = req.headers.authorization.split("Bearer ")[1];
+  } else {
+    console.error("No token found");
+    return res.status(403).json({ error: "Unauthorized" });
+  }
+
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then(decodedToken => {
+      req.user = decodedToken;
+      return db
+        .collection("users")
+        .where("userId", "==", req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then(data => {
+      req.user.handle = data.docs[0].data().handle;
+      return next();
+    })
+    .catch(err => {
+      console.log(err);
+      return res.status(403).json(err);
+    });
+};
+
 // Create data
-app.post("/screams", (req, res) => {
+app.post("/screams", FBAuth, (req, res) => {
   const newScream = {
     body: req.body.body,
-    userHandle: req.body.userHandle,
+    userHandle: req.user.handle,
     createdAt: new Date().toISOString()
   };
 
@@ -67,6 +100,17 @@ app.post("/screams", (req, res) => {
     });
 });
 
+const isEmail = email => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!email.match(regEx)) return true;
+  else return false;
+};
+
+const isEmpty = string => {
+  if (string.trim() === "") return true;
+  else return false;
+};
+
 // Register User
 app.post("/register", (req, res) => {
   const newUser = {
@@ -75,6 +119,21 @@ app.post("/register", (req, res) => {
     confirmPassword: req.body.confirmPassword,
     handle: req.body.handle
   };
+
+  let errors = {};
+
+  if (isEmpty(newUser.email)) {
+    errors.email = "Must not be empty";
+  } else if (isEmail(newUser.email)) {
+    errors.email = "Must be a valid email address";
+  }
+
+  if (isEmpty(newUser.password)) errors.password = "Must not be empty";
+  if (newUser.password !== newUser.confirmPassword)
+    errors.confirmPassword = "Password must match";
+  if (isEmpty(newUser.handle)) errors.handle = "Must not be empty";
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
   let token, userId;
 
@@ -126,6 +185,40 @@ app.post("/register", (req, res) => {
           error: err.code
         });
       }
+    });
+});
+
+// Login route
+app.post("/login", (req, res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password
+  };
+
+  let errors = {};
+
+  if (isEmpty(user.email)) return (errors.email = "Must not be empty");
+  if (isEmpty(user.password)) return (errors.password = "Must not be empty");
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+    .then(data => {
+      return data.user.getIdToken();
+    })
+    .then(token => {
+      return res.json({ token });
+    })
+    .catch(err => {
+      console.log(err);
+      if (err.code === "auth/wrong-password") {
+        return res
+          .status(403)
+          .json({
+            general:
+              "The password is invalid or the user does not have a password."
+          });
+      } else return res.status(500).json({ error: err.code });
     });
 });
 
