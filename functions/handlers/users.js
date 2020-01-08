900197319f667455d952a587a221329e2a956995
BennoAlif <bennoalif41@gmail.com>
2020-01-08 16:58:14 +0700

    Image Upload

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -1,6 +1,9 @@
-const { db } = require("../util/admin");
+const { admin, db } = require("../util/admin");
 const firebase = require("firebase");
-const { validateRegisterData, validateLoginData } = require("../util/validators");
+const {
+  validateRegisterData,
+  validateLoginData
+} = require("../util/validators");
 
 const config = require("../util/config");
 firebase.initializeApp(config);
@@ -14,7 +17,9 @@ exports.register = (req, res) => {
   };
 
   const { valid, errors } = validateRegisterData(newUser);
-  if(!valid) return res.status(400).json(errors)
+  if (!valid) return res.status(400).json(errors);
+
+  const noImg = "no-image.png";
 
   let token, userId;
 
@@ -46,7 +51,8 @@ exports.register = (req, res) => {
         userId,
         email: newUser.email,
         handle: newUser.handle,
-        createdAt: new Date().toISOString()
+        createdAt: new Date().toISOString(),
+        imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${noImg}?alt=media`
       };
 
       //   Set document user
@@ -76,7 +82,7 @@ exports.login = (req, res) => {
   };
 
   const { valid, errors } = validateLoginData(user);
-  if(!valid) return res.status(400).json(errors)
+  if (!valid) return res.status(400).json(errors);
 
   firebase
     .auth()
@@ -96,4 +102,59 @@ exports.login = (req, res) => {
         });
       } else return res.status(500).json({ error: err.code });
     });
+};
+
+exports.uploadImage = (req, res) => {
+  const BusBoy = require("busboy");
+  const path = require("path");
+  const os = require("os");
+  const fs = require("fs");
+
+  const busboy = new BusBoy({ headers: req.headers });
+
+  let imageToBeUploaded = {};
+  let imageFileName;
+
+  busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+    if (mimetype !== "image/jpeg" && mimetype !== "image/png") {
+      return res.status(400).json({ message: "Wrong file type" });
+    }
+
+    const imageExtension = filename.split(".")[filename.split(".").length - 1];
+
+    imageFileName = `${Math.round(
+      Math.random() * 1000000000000
+    ).toString()}.${imageExtension}`;
+
+    const filepath = path.join(os.tmpdir(), imageFileName);
+
+    imageToBeUploaded = { filepath, mimetype };
+
+    file.pipe(fs.createWriteStream(filepath));
+  });
+  busboy.on("finish", () => {
+    admin
+      .storage()
+      .bucket()
+      .upload(imageToBeUploaded.filepath, {
+        resumable: false,
+        metadata: {
+          metadata: {
+            contentType: imageToBeUploaded.mimetype
+          }
+        }
+      })
+      .then(() => {
+        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`;
+        return db.doc(`/users/${req.user.handle}`).update({ imageUrl });
+      })
+      .then(() => {
+        return res.json({ message: "Image uploaded" });
+      })
+      .catch(err => {
+        console.log(err);
+        return res.status(500).json({ error: err.code });
+      });
+  });
+  busboy.end(req.rawBody);
 };
--- functions/index.js
+++ functions/index.js
@@ -2,9 +2,8 @@ const functions = require("firebase-functions");
 const express = require("express");
 
 const { getAllScreams, postOneScream } = require("./handlers/screams");
-const {register, login} = require("./handlers/users")
-const FBAuth = require('./util/fbAuth')
-
+const { register, login, uploadImage } = require("./handlers/users");
+const FBAuth = require("./util/fbAuth");
 
 const app = express();
 
@@ -20,5 +19,7 @@ app.post("/register", register);
 // Login route
 app.post("/login", login);
 
+// Upload user image
+app.post("/user/image", FBAuth, uploadImage);
 
 exports.api = functions.https.onRequest(app);
