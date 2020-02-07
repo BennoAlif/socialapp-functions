b9e0edeac6941448db97996e24cc90741fa9a49f
BennoAlif <bennoalif41@gmail.com>
2020-02-07 18:28:25 +0700

    bug di login validation

--- functions/util/validators.js
+++ functions/util/validators.js
@@ -32,8 +32,8 @@ exports.validateRegisterData = data => {
 exports.validateLoginData = data => {
   let errors = {};
 
-  if (isEmpty(data.email)) return (errors.email = "Must not be empty");
-  if (isEmpty(data.password)) return (errors.password = "Must not be empty");
+  if (isEmpty(data.email)) errors.email = "Must not be empty";
+  if (isEmpty(data.password)) errors.password = "Must not be empty";
 
   return {
     errors,
